108bf83d1efacb617a2d1013ffc8882d0a429295
EranKT <eran@ktfgalaxy.com>
2022-09-03 18:00:30 +0200

    added second get route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from "@nestjs/common";
 
-@Controller()
+@Controller('/app')
 export class AppController {
 
-    @Get()
+    @Get('/asdf')
     getRootRoute() {
         return 'Get api call answer';
+    }
+
+    @Get('/bye')
+    getByeThere() {
+        return 'bye there';
     }
 }
